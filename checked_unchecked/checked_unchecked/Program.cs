90a4330759b55195cbe5fbd7d44dd8d33bb86393
overkektus <egor.piskunou@gmail.com>
2017-03-08 13:59:21 +0300

    checked unchecked

---
+++ checked_unchecked/checked_unchecked/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte a, b, result;
+            Console.Write("Введите количество опросов: ");
+            int i = int.Parse(Console.ReadLine());
+
+            for (int j = 1; j <= i; j++)
+            {
+                try
+                {
+                    Console.Write("Введите a: ");
+                    // Используем unchecked в одном выражении
+                    a = unchecked((byte)int.Parse(Console.ReadLine()));
+                    Console.Write("Введите b: ");
+                    b = unchecked((byte)int.Parse(Console.ReadLine()));
+
+                    // Используем checked для всего блока операторов
+                    checked
+                    {
+                        result = (byte)(a + b);
+                        Console.WriteLine("a + b = " + result);
+                        result = (byte)(a * b);
+                        Console.WriteLine("a*b = " + result + "\n");
+                    }
+                }
+                catch (OverflowException)
+                {
+                    Console.Write("Переполнение\n\n");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
