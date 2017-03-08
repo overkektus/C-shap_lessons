937032daf8d32849844f76b6a477addb0fbb1ea7
overkektus <egor.piskunou@gmail.com>
2017-03-08 15:00:36 +0300

    dispose and finalize

---
+++ dispose_finalize/dispose_finalize/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dispose_finalize
+{
+    // Данный класс реализует интерейс IDisposable
+    class FinalizeObject : IDisposable
+    {
+        public int id { get; set; }
+
+        public FinalizeObject(int id)
+        {
+            this.id = id;
+        }
+
+        // Реализуем метод Dispose()
+        public void Dispose()
+        {
+            Console.WriteLine("Высвобождение объекта!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FinalizeObject obj = new FinalizeObject(4);
+            obj.Dispose();
+
+            Console.Read();
+        }
+    }
+}
