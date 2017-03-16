dfe7b3de03b9c2bc7ffbeb3df6157eebd261cb84
overkektus <egor.piskunou@gmail.com>
2017-03-16 23:40:09 +0300

    Generalized methods
    1

---
+++ Generalized methods/Generalized methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generalized_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method(5, 10);
+            Method("afsgsg", "fshfd");
+        }
+
+        static T Method<T>(T x, T y)
+        {
+            Console.WriteLine("Первый параметр x = {0}, второй параметр y = {1}", x,y);
+            Console.ReadLine();
+            return x;
+        }
+    }
+}
