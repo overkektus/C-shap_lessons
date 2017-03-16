b37857133cf77a8a981aaa4b4f102fefb29ec25b
overkektus <egor.piskunou@gmail.com>
2017-03-16 20:29:34 +0300

    generic List

---
+++ generic/generic/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<byte> mass = new List<byte>(); //обобщенный список для работы с типом byte
+            mass.Add(100);
+
+            byte x = mass[0];
+
+            List<SomeClass> massUser = new List<SomeClass>();
+
+            List<int> mass2 = new List<int>();
+            mass2.Add(1);
+            mass2.Add(5);
+            mass2.Add(12);
+
+            for (int i = 0; i < mass2.Count; i++)
+                Console.WriteLine(mass2[i]);
+
+            massUser.Add(new SomeClass(1));
+            massUser.Add(new SomeClass(5));
+            massUser.Add(new SomeClass(12));
+
+            for (int j = 0; j < massUser.Count; j++)
+                Console.WriteLine(massUser[j].x);
+        }
+    }
+}
+
+class SomeClass
+{
+    public int x;
+    public SomeClass(int input)
+    {
+        x = input;
+    }
+}
