0d91dbca7079b7932a38a14f749c572d9aa7b6c2
overkektus <egor.piskunou@gmail.com>
2017-03-24 19:42:21 +0300

    1

---
+++ Generic-classes-and-interfaces/Generic-classes-and-interfaces/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_classes_and_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+class SomeClass : IsomeInterface<string>
+{
+    public string SomeMethod(string input1, string input2)
+    {
+        return input1 + " " + input2;
+    }
+    public void ShowData(string input)
+     {
+        Console.WriteLine(input.ToUpper());
+     }
+}
+
+public interface IsomeInterface<T>
+{
+    T SomeMethod(T input1, T input2);
+    void ShowData(T input);
+}
+
+
+class SomeAnyClass : IsomeInterface<int>
+{
+    public int SomeMethod(int input1, int input2)
+    {
+        return input1 + input2 - 3;
+    }
+
+    public void ShowData(int input)
+    {
+        Console.WriteLine(input.ToString());
+    }
+}
