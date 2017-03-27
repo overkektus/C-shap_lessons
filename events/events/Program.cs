38a31438f3874fe7c9cf43674fe0425ef0f7c69a
overkektus <egor.piskunou@gmail.com>
2017-03-27 19:27:12 +0300

    events

---
+++ events/events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace events
+{
+    delegate void PushPrinterButton(); //Объявление делегата
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mouse mouse = new Mouse();
+            // подписка на событие с указанием конкретного метода
+            mouse.Click += new PushPrinterButton(mouse.OnMouseClick);
+            //.......................
+            mouse.DoEvent(); // вызывается событие
+        }
+    }
+
+    class Mouse
+    {
+        public event PushPrinterButton Click; // объявление события о нажатии кнопки мыши
+        public void DoEvent() // генерация события
+        {
+            if (Click != null)
+                Click(); // запуск события
+        }
+        public void OnMouseClick()
+        {
+            Console.WriteLine("Произошло нажатие кнопки мыши по кнопке печати.\n");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ group_delegate/group_delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace group_delegate
+{
+    delegate void Del();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Del del = new Del(M1);
+            del += M2;
+            del += M1;
+            del -= M2;
+            MDel(ref del);
+        }
+        static void M1()
+        {
+            Console.WriteLine("Это метод M1");
+            Console.ReadLine();
+        }
+        static void M2()
+        {
+            Console.WriteLine("Этот метод M2");
+            Console.ReadLine();
+        }
+        static void MDel(ref Del d)
+        {
+            Console.WriteLine("Передача делегата в качестве параметра");
+            d();
+        }
+    }
+}
