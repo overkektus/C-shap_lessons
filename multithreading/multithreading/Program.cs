5dc277f66d73e4e54436b38359238a3c207445ac
overkektus <egor.piskunou@gmail.com>
2017-04-03 21:56:41 +0300

    multithreading

---
+++ multithreading/multithreading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass sc = new SomeClass();
+
+            Thread thread = new Thread(sc.JustMethod);
+            thread.Start();
+
+            sc.JustMethod();
+            Console.Read();
+        }
+    }
+}
+
+
+class SomeClass
+{
+    bool flag = false;
+
+    public void JustMethod()
+    {
+        Console.WriteLine("flag = {0}", flag);
+        flag = !flag;
+    }
+}
